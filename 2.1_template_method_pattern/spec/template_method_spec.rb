a1a6eaef21c9bae7e183e4ef2f8cd566effb0167
Toth Peter <tothpeter08@gmail.com>
2015-02-15 20:38:18 +0100

    Special abilities and Mage occupation

--- 2.1_template_method_pattern/spec/template_method_spec.rb
+++ 2.1_template_method_pattern/spec/template_method_spec.rb
@@ -17,5 +17,20 @@ describe "Template Method Pattern" do
     it "has default demage rating of 15" do
       expect(hero.damage).to eq(15)
     end
+
+    it "has Strike special ability" do
+      expect(hero.abilities).to include(:strike)
+    end
+  end
+
+  context "Mage" do
+    let(:hero) { Hero.new :mage }
+    it "has default demage rating of 7" do
+      expect(hero.damage).to eq(7)
+    end
+
+    it "has Strike special ability" do
+      expect(hero.abilities).to include(:magic_arrow)
+    end
   end
 end
