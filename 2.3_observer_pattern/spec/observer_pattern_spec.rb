da467a5c135f989d01be4c1229acaf01cc4254cd
Toth Peter <tothpeter08@gmail.com>
2015-02-16 19:57:16 +0100

    Activate curse - smelly approach

--- 2.3_observer_pattern/spec/observer_pattern_spec.rb
+++ 2.3_observer_pattern/spec/observer_pattern_spec.rb
@@ -4,8 +4,9 @@ describe "Observer Pattern" do
   # Integration tests
   
   describe Hero do
+    let(:hero) { hero = Hero.new }
+
     it "is cursed when discovers cursed Tile" do
-      hero = Hero.new
       tile = Tile.new cursed: true
       hero.discover tile
 
@@ -14,15 +15,32 @@ describe "Observer Pattern" do
     end
 
     it "is not cursed when discovers simple Tile without curse" do
-      hero = Hero.new
       tile = Tile.new
       hero.discover tile
 
       expect(hero.cursed?).to be false
     end
+
+    it "has default health equal to 10" do
+      expect(hero.health).to eq(10)
+    end
+
+    it "can be famaged" do
+      hero.damage 6
+      expect(hero.health).to eq(4)
+    end
   end
 
   describe Tile do
+
+    it "activates curse" do
+      hero = Hero.new
+      tile = Tile.new cursed: true, hero: hero
+      
+      tile.activate_curse
+      expect(hero.health).to eq(6)
+    end
+
     it "is no cursed by default" do
       tile = Tile.new
       expect(tile.cursed?).to be false
