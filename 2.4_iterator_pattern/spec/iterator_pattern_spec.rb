8f537538e33578c5b836fe0692d8c581e1f07f7e
Toth Peter <tothpeter08@gmail.com>
2015-02-16 21:20:58 +0100

    External iterator

--- 2.4_iterator_pattern/spec/iterator_pattern_spec.rb
+++ 2.4_iterator_pattern/spec/iterator_pattern_spec.rb
@@ -2,17 +2,29 @@ require "iterator_pattern"
 
 describe "Iterator Pattern" do
   describe Inventory do
-    it "adds items to its collection" do
+    let(:inventory) { Inventory.new }
+
+    before :each do
       item1 = Item.new
       item1.cost = 20
 
       item2 = Item.new
       item2.cost = 10
 
-      inventory = Inventory.new
       inventory.add item1
       inventory.add item2
+    end
 
+    it "can be iterated through items to get total cost" do
+      # External itarator
+      iterator = InventoryIterator.new inventory
+      result = 0
+      while iterator.has_next?
+        result += iterator.next.cost
+      end
+      expect(result).to eq(30)
+    end
+    it "adds items to its collection" do
       expect(inventory.items.size).to eq(2)
     end
   end
