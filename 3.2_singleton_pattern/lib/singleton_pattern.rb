4386d0ac6cc0f8807b002d302372735f30a3360f
Toth Peter <tothpeter08@gmail.com>
2015-02-19 22:31:05 +0100

    Singleton ruby library

--- 3.2_singleton_pattern/lib/singleton_pattern.rb
+++ 3.2_singleton_pattern/lib/singleton_pattern.rb
@@ -1,4 +1,19 @@
+require "singleton"
+
 class HeroFactory
+
+  include Singleton
+
+  def create_warrior
+    Warrior.new
+  end
+
+  def create_mage
+    Mage.new
+  end
+end
+
+class HeroFactory2
 
   def self.instance
     @@instance ||= self.send :new
