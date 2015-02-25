767580e07cbd7b66cee338e8f830969624ce528c
Toth Peter <tothpeter08@gmail.com>
2015-02-25 21:47:06 +0100

    Authorisation with proxy

--- 4.3_proxy_pattern/lib/proxy_pattern.rb
+++ 4.3_proxy_pattern/lib/proxy_pattern.rb
@@ -1,11 +1,31 @@
 require "forwardable"
 
+class Hero
+  attr_accessor :keywords
+
+  def initialize
+    @keywords = []
+  end
+end
+
 class ComputerProxy
   extend Forwardable
 
-  def_delegators :@real_object, :add, :execute
-  def initialize real_object
+  def_delegators :@real_object, :add
+  def initialize real_object, hero
     @real_object = real_object
+    @hero = hero
+  end
+
+  def execute
+    check_access
+    @real_object.execute
+  end
+
+  def check_access
+    unless @hero.keywords.include? :computer
+      raise 'You have no access'
+    end
   end
 
   # def method_missing name, *args
--- 4.3_proxy_pattern/spec/proxy_pattern_spec.rb
+++ 4.3_proxy_pattern/spec/proxy_pattern_spec.rb
@@ -4,12 +4,15 @@ describe "Proxy Pattern" do
 
   it "delegates all functionality to the real object" do
     # Stub functions
+    hero = Hero.new
     computer = double("computer", queue: [], add: [], execute: true)
-    proxy = ComputerProxy.new computer
+    proxy = ComputerProxy.new computer, hero
 
     expect(computer).to receive :add 
     proxy.add(double("command"))
 
+    
+    hero.keywords << :computer
     # Old syntax
     # computer.should_receive :execute
     # New
